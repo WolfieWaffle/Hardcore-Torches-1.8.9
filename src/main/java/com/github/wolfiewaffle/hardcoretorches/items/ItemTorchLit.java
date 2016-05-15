0229b3bd5491a4244906be3fe9458c97bbaacd0a
WolfieWaffle <wolfie.waffle11@gmail.com>
2016-05-15 12:01:38 -0600

    The spelling on that last commit was horrible

--- src/main/java/com/github/wolfiewaffle/hardcoretorches/items/ItemTorchLit.java
+++ src/main/java/com/github/wolfiewaffle/hardcoretorches/items/ItemTorchLit.java
@@ -4,6 +4,7 @@ import net.minecraft.block.Block;
 import net.minecraft.item.ItemBlock;
 
 public class ItemTorchLit extends ItemBlock {
+	// Constructor
 	public ItemTorchLit(Block block) {
 		super(block);
 		this.setRegistryName("torch_lit");
