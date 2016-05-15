6ca246fef4b178a7c589100eaadcd54a0c6c3229
WolfieWaffle <wolfie.waffle11@gmail.com>
2016-05-15 12:00:19 -0600

    Remove a commetn that might now be right

--- src/main/java/com/github/wolfiewaffle/hardcoretorches/items/ItemTorchLit.java
+++ src/main/java/com/github/wolfiewaffle/hardcoretorches/items/ItemTorchLit.java
@@ -4,14 +4,8 @@ import net.minecraft.block.Block;
 import net.minecraft.item.ItemBlock;
 
 public class ItemTorchLit extends ItemBlock {
-	/*
-	 *  Constructor must be passed a valid Block for registration,
-	 *  this is the ItemBlock version of that Block
-	 */
 	public ItemTorchLit(Block block) {
 		super(block);
 		this.setRegistryName("torch_lit");
 	}
-
-	
 }
